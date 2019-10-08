b8b08891620069ed507fd327e79b028b4956b293
marianaLz <marianaglp15@gmail.com>
2019-10-08 10:18:55 -0500

    Initial configuration

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DB || "mongodb://localhost/jaya-tech-challenge", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+const app = express();
+
+// Middleware Setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const index = require("./routes/index");
+app.use("/", index);
+
+module.exports = app;
